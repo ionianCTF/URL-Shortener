8fd8ccedb74d4e8d2dae28b5e803e4cbfee3c92f
Haki-Malai <p17mala@ionio.gr>
2022-02-21 20:36:20 +0200

    requests, sqlite, base62, class Handler and more...

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
 const port = 3001;
 var cors = require('cors');
 
-app.use(cors())
-app.get('/', (req, res) => {
-  res.send({message: 'Hello World!'})
-})
+const Handler = require('./handler.js');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/:url', (req, res) => {
+    res.send({ message: Handler.redirect(req.params.url) })
+});
+
+
+router.post('/add/:new_url', (req, res) => {
+    console.log(req.params.new_url);
+});
+
+app.use("/", router);
