0421adfd0f8ca66fd66fe798ae0f66c058449458
Haki-Malai <p17mala@ionio.gr>
2022-02-25 16:00:52 +0200

    Finished react app

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,23 +1,62 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 
 import './App.css';
-import Input from './Components/input';
-import Form from 'react-bootstrap/Form';
+import Input from './Components/input.js';
+import Error from './Components/urlError.js';
+import Success from './Components/success.js';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            testMessage: null
+            status: 205,
+            last : {
+                url : '',
+                redirects : ''
+            }
+        };
+        this.setStatus = this.setStatus.bind(this);
+    }
+    setStatus(code, last) {
+        /*200 OK
+          205 Reset Content
+          404 Not Found
+          406 Not Acceptable
+        */
+        if (last) {
+            this.setState( (state, props) => ({
+                status: code,
+                last : { 
+                    url : last.url,
+                    redirects : last.redirects
+                }
+            }));
+        } else {
+            this.setState( (state, props) => ({
+                status: code
+            }));
+          
         }
     }
     render() {
+        var toRender;
+        if (this.state.status == 205) {
+            toRender = <Input setStatus = { this.setStatus } />;
+        } else if (this.state.status == 406) {
+            toRender = <Error setStatus = { this.setStatus } />;
+        } else if (this.state.status == 200) {
+            toRender = <Success url = { this.state.last.url } redirects = { this.state.last.redirects } setStatus = { this.setStatus } />;
+        }
         return (
+            <div>
             <Form className="text-center">
                 <Form.Label className="display-1 fw-bold font-monospace">Short URL</Form.Label>
-                <Input message = { this.state.testMessage } />
             </Form>
+            {toRender}
+            </div>
         );
     }
 }
--- reactjs/src/Components/input.js
+++ reactjs/src/Components/input.js
@@ -9,14 +9,53 @@ import Form from 'react-bootstrap/Form';
 class Input extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            link : ''
+        };
+        this.addUrl = this.addUrl.bind(this);
+    }
+    addUrl() {
+        //fetch('https://url-shortener-p17.herokuapp.com/5')
+        //    .then(response => response.json())
+        //   .then((json) => {
+        //        console.log( json.url )
+        //    })
+        const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+        const regex = new RegExp(expression);
+
+        if (this.state.link.match(regex)) {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ url : this.state.link })
+            };
+
+            fetch('https://url-shortener-p17.herokuapp.com/new', requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    const last = {
+                        url : data.url,
+                        redirects : this.state.link
+                    }
+                    this.props.setStatus(200, last);
+                });
+        } else {
+            this.props.setStatus(406);
+        }
     }
     render() {
         return (
             <Container className="vertical-center bg-dark p-4 rounded shadow-lg">
-                <Form.Label htmlFor="basic-url" className="display-6 text-light">Paste the URL to be shortened</Form.Label>
+                <Form.Label htmlFor="basic-url" className="display-6 text-light" value={this.state.link}>Paste the URL to be shortened</Form.Label>
                 <InputGroup className="mb-3">
-                    <FormControl placeholder="Enter the link here" aria-label="Enter the link here" aria-describedby="basic-describedby" />
-                    <Button variant="outline-secondary">Shorten URL</Button>
+                    <FormControl 
+                        placeholder="Enter the link here" 
+                        aria-label="Enter the link here" 
+                        aria-describedby="basic-describedby"
+                        value={this.state.val}
+                        onChange={e => this.setState({ link: e.target.value })}
+ />
+                    <Button onClick={this.addUrl} variant="outline-secondary">Shorten URL</Button>
                 </InputGroup>
             </Container>
         );
